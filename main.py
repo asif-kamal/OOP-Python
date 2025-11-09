e79a4183ae7bdf432e263fdc2a530e028c20f447
asif-kamal <asif.kamal1489@gmail.com>
2025-11-08 19:55:51 -0600

    completed OOP lesson w/ Turtle library

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+sally = turtle.Turtle()
+sally.shape("turtle")
+sally.color("red")
+
+sally.forward(100)
+
+my_screen = turtle.Screen()
+my_screen.bgcolor("teal")
+my_screen.exitonclick()
